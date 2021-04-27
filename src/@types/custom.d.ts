da3ea890b25dd0359f98933cabe58246b06facef
Otacilio Neto <otaciliomanoel.neto@hotmail.com>
2021-04-27 12:33:19 -0300

    desafio completo :tada:

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -1,1 +1,29 @@
-declare module '*.svg'
+declare module '*.svg';
+
+interface GenreResponseProps {
+	id: number;
+	name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+	title: string;
+}
+
+interface MovieProps {
+	imdbID: string;
+	Title: string;
+	Poster: string;
+	Ratings: Array<{
+		Source: string;
+		Value: string;
+	}>;
+	Runtime: string;
+}
+
+interface SidebarProps {
+	genres: GenreResponseProps[];
+	handleClick: (number: number) => void;
+	selectedGenreId: number;
+}
+
+interface ContentProps {
+	selectedGenre: GenreResponseProps;
+	movies: MovieProps[];
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,21 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+import { Button } from './Button';
+
+export function SideBar({ genres, handleClick, selectedGenreId }: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
